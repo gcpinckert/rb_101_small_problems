f86baa4e5cf17dc590dbab81ed65cd43213d8aaa
Ginni Pinckert <gcpinckert@gmail.com>
2021-03-10 15:13:38 -0800

    Refactor Easy 2: 8

--- easy_2/08.rb
+++ easy_2/08.rb
@@ -44,34 +44,61 @@ The product of the integers between 1 and 6 is 720.
 - Display results
 =end
 
-end_number = ''
-until end_number.to_i.to_s == end_number && end_number.to_i > 0
-  puts "Please enter an integer greater than 0:"
-  end_number = gets.chomp
+def prompt(msg)
+  puts ">> #{msg}"
 end
 
-which_calc = ''
-until ['s', 'p'].include?(which_calc)
-  puts "Enter 's' to compute the sum, 'p' to compute the product."
-  which_calc = gets.chomp
+def get_valid_number
+  end_number = ''
+  until end_number.to_i.to_s == end_number && end_number.to_i > 0
+    prompt "Please enter an integer greater than 0:"
+    end_number = gets.chomp
+  end
+  end_number.to_i
 end
+
+def get_calc_type
+  which_calc = ''
+  until ['s', 'p'].include?(which_calc)
+    prompt "Enter 's' to compute the sum, 'p' to compute the product."
+    which_calc = gets.chomp
+  end
+  which_calc
+end
+
+def calc_sum(end_number)
+  # sum = 0
+  # counter = 1
+  # while counter <= end_number.to_i
+  #   sum += counter
+  #   counter += 1
+  # end
+
+  # sum
+
+  (1..end_number).inject(:+)
+end
+
+def calc_product(end_number)
+  # product = 1
+  # counter = 1
+  # while counter <= end_number.to_i
+  #   product *= counter
+  #   counter += 1
+  # end
+
+  # product
+
+  (1..end_number).inject(:*)
+end
+
+end_number = get_valid_number
+which_calc = get_calc_type
 
 if which_calc == 's'
-  sum = 0
-  counter = 1
-  while counter <= end_number.to_i
-    sum += counter
-    counter += 1
-  end
-  puts "The sum of the integers between 1 and #{end_number} is #{sum}."
+  sum = calc_sum(end_number)
+  prompt "The sum of the integers between 1 and #{end_number} is #{sum}."
 else
-  product = 1
-  counter = 1
-  while counter <= end_number.to_i
-    product *= counter
-    counter += 1
-  end
-  puts "The product of the integers between 1 and #{end_number} is #{product}."
+  product = calc_product(end_number)
+  prompt "The product of the integers between 1 and #{end_number} is #{product}."
 end
-
-
